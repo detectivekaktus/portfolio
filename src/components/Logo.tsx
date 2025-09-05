1629d3743e85b4db36fe3c583e8edc68480b88ee
Artiom Astashonak <artiomastashonak@gmail.com>
2025-09-05 12:14:36 +0200

    Use semantic HTML for `Logo` component.

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,9 +2,9 @@ import "../css/Logo.css";
 
 export function Logo() {
   return (
-    <div className="logo">
-      <h1>Artiom Astashonak</h1>
-      <p>Software Developer</p>
+    <div>
+      <p className="logo-title">Artiom Astashonak</p>
+      <p className="logo-subtitle">Software Developer</p>
     </div>
   );
 }
